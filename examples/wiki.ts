55320fc046fd4191806889a92037d0ff97ec6dee
Abiria <oro3673@gmail.com>
2025-11-13 06:23:11 -0500

    update wiki example

--- examples/wiki.ts
+++ examples/wiki.ts
@@ -1,18 +1,50 @@
-import { alt, match, repeat0, until, SpannedString } from '../src/index'
+import {
+	alt,
+	repeat0,
+	SpannedString,
+	spanHighlighterStream,
+	p,
+	charNoneOfRepeat1,
+	seq,
+	type Pattern,
+	charOneOf,
+	negativeLookahead,
+	renderMermaid,
+} from '../src/index'
 
-const llbra = match('[[')
-const rrbra = match(']]')
+const ll = p('ll', '[[', true)
+const rr = p('rr', ']]', true)
 
-const word = until(']]')
+const specialCharacters = '[]'
 
-const markupBase = alt(word, llbra, rrbra)
+const word = p('word', charNoneOfRepeat1(specialCharacters), true)
 
-const markup = repeat0(markupBase)
+const special = charOneOf(specialCharacters)
 
-const text = SpannedString.from(
-	`제 1조. [[대한민국]]은 [[민주주의|민주]][[공화국]]이며`,
-)
+let markup: Pattern
+
+const linkInner = repeat0(alt(word, negativeLookahead(special, rr)))
+
+const link = p('link', source => seq(ll, linkInner, rr)(source))
+
+const markupBase = alt(link, word, special)
+
+markup = p('markup', repeat0(markupBase))
+
+// const raw = `제 1조. [[대한민국]]은 [[민주주의|민주]][[공화국]]이며`
+const raw = `aaaa]] bdfjddfd [[  ajdfjak]  adfkaj  dafja [ dajfa ] 1]] dkjfa ]] kk [[ kjha`
+
+const text = SpannedString.from(raw)
 
 const parsed = markup(text)
 
-console.log(parsed)
+if (parsed === null) throw new Error('parse failed')
+
+console.log(spanHighlighterStream(parsed)?.trim())
+// console.log(renderMermaid(parsed))
+// console.log(
+// 	renderMermaid({
+// 		...parsed,
+// 		tree: parsed.tree.projectToNamed(),
+// 	}),
+// )
