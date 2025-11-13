1c4e4a60ee1b231874e00623de51e4d115ada79a
Abiria <oro3673@gmail.com>
2025-11-13 03:54:36 -0500

    support regex

--- src/combinator.ts
+++ src/combinator.ts
@@ -8,12 +8,30 @@ declare global {
 	}
 }
 
-export const match =
+export const match = (pattern: string | RegExp) =>
+	typeof pattern === 'string' ? matchString(pattern) : matchRegex(pattern)
+
+export const matchString =
 	(lit: string): Pattern =>
 	source =>
 		source.window.startsWith(lit)
 			? newLeafSuccessResult(source, lit.length)
 			: null
+
+export const matchRegex = (re: RegExp): Pattern => {
+	if (!re.source.startsWith('^')) {
+		throw new Error('matchRegex only accepts regex starting with ^')
+	}
+
+	return source => {
+		const match = re.exec(source.window)
+		if (match === null) return null
+
+		const [matched] = match
+
+		return newLeafSuccessResult(source, matched.length)
+	}
+}
 
 export const repeat0 =
 	(pattern: Pattern): Pattern =>
