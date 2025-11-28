189b7ac05b80b333681da2e26925f2394dd44f7c
Abiria <oro3673@gmail.com>
2025-11-28 05:02:35 -0500

    make calc demo be actually computable

--- examples/calc.ts
+++ examples/calc.ts
@@ -1,4 +1,12 @@
-import { alt, assocBinLeftMap, match } from '@/combinators'
+import {
+	alt,
+	assocBinLeftMap,
+	enclosedBy,
+	mapSuccess,
+	match,
+	parenthesized,
+	spTabNl0,
+} from '@/combinators'
 import { p, pMap } from '@/pattern-utils'
 import { SpannedString } from '@/spanned-string'
 import { rec, renderMermaid } from '@/utils'
@@ -7,30 +15,63 @@ const { expr } = rec({
 	expr: $ => alt($.addsub),
 
 	addsub: $ =>
-		assocBinLeftMap(
-			$.muldiv,
-			[p('+', '+', true), pMap('add')],
-			[p('-', '-', true), pMap('sub')],
-		),
+		assocBinLeftMap($.muldiv, ['+', pMap('add')], ['-', pMap('sub')]),
 
 	muldiv: $ =>
-		assocBinLeftMap(
-			$.num,
-			[p('*', '*', true), pMap('mul')],
-			[p('/', '/', true), pMap('div')],
-		),
+		assocBinLeftMap($.atom, ['*', pMap('mul')], ['/', pMap('div')]),
 
-	num: $ => p('num', match(/^-?(0|[1-9]\d*)/)),
+	atom: $ => enclosedBy(alt($.num, parenthesized($.expr)), spTabNl0),
+
+	num: $ =>
+		p(
+			'num',
+			mapSuccess(/^-?(0|[1-9]\d*)/, mr => {
+				mr.tree.meta ??= {}
+				mr.tree.meta.value = parseInt(mr.tree.spanned.window)
+
+				return mr
+			}),
+		),
 })
 
-const source = SpannedString.from(`1+2*3-4/5`)
+const source = SpannedString.from(`1 + 2 * -3 - 4 / 5 * (-6 + 11)`)
 
 const result = expr(source)
+
 if (result === null) throw 1
 
-console.log(
-	renderMermaid({
-		...result,
-		tree: result.tree.projectToNamed(),
-	}),
-)
+// console.log(
+// 	renderMermaid({
+// 		...result,
+// 		tree: result.tree.projectToNamed(),
+// 	}),
+// )
+
+const projectedTree = result.tree.projectToNamed()
+
+for (const tree of projectedTree.iterLeft()) {
+	if (tree.name === 'num') continue
+	if (tree === projectedTree) {
+		tree.meta = tree.meta ?? {}
+		tree.meta.value = tree.children?.[0]?.meta?.value as number
+		continue
+	}
+
+	const [lhs, rhs] = tree.children
+	if (!lhs || !rhs) throw new Error(`invalid tree structure: ${tree.name}`)
+	const lval = lhs.meta?.value as number
+	const rval = rhs.meta?.value as number
+
+	let value = 0
+
+	if (tree.name === 'add') value = lval + rval
+	else if (tree.name === 'sub') value = lval - rval
+	else if (tree.name === 'mul') value = lval * rval
+	else if (tree.name === 'div') value = lval / rval
+	else throw new Error(`unknown operator: ${tree.name}`)
+
+	tree.meta = tree.meta ?? {}
+	tree.meta.value = value
+}
+
+console.log('result:', projectedTree.meta?.value)
